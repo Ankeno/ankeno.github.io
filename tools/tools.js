93749aeb7bd51ea28b9b82a3de5808c8ab60bf09
Ankeno <andrei.calidar@gmail.com>
2020-05-15 14:27:59 +0300

    updated tools

--- tools/tools.js
+++ tools/tools.js
@@ -5,37 +5,8 @@ function pcalc() {
 
     let r = Number(v) / Number(t) * 100;
 
-    document.getElementById('pcalcr').innerHTML = 'Result: ' + r + '%';
+    let rn = Math.round(r * 10) / 10;
 
-};
+    document.getElementById('pcalcr').innerHTML = 'Result: ' + rn + '%';
 
-var t1 = [
-    {
-        name: 'Item1',
-        items: ['Red', 'Blue', 'Green']
-    },
-    {
-        name: 'Item2',
-        items: ['Green', 'Purple', 'Blue']
-    }
-];
-
-var t2 = [
-    {
-        name: 'Item1',
-        items: ['Red', 'Blue', 'Green']
-    },
-    {
-        name: 'Item2',
-        items: ['Green', 'Purple', 'Blue']
-    }
-];
-
-
-function stuff() {
-    let t = t1[1].items[1];
-
-
-
-    document.getElementById('out').innerHTML = t;
 };
