18b8170b124693672480b22ecb5007714190d477
Ankeno <andrei.calidar@gmail.com>
2020-08-30 21:46:38 +0300

    dice type update

--- dice/dice.js
+++ dice/dice.js
@@ -7,26 +7,53 @@ function clock() {
     m = clockNum(m);
     document.getElementById("clock").innerHTML = h + ":" + m;
     var t = setTimeout(clock, 1000);
-}
+};
 
 function clockNum(c) {
-    if (c < 10) {c = "0" + c};
+    if (c < 10) { c = "0" + c };
     return c;
-}
+};
 
 // dice roll
 function randomDice(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 function diceRoll() {
     let diceNumber = document.querySelector('input[name = "dice"]:checked').value;
     let diceContainer = document.getElementById('diceBoard');
+    let diceType = document.querySelector('input[name = "diceT"]:checked').value;
+    let diceMaxDots = 0;
+
+    if (diceType == 'Classic') {
+        diceMaxDots = 6;
+    } else {
+        diceMaxDots = 9;
+    };
 
     diceContainer.innerHTML = '';
 
+    if (diceNumber > 2) {
+        let dRow = document.createElement('div');
+        dRow.setAttribute('class', 'boardRow');
+        dRow.setAttribute('id', 'boardRow1');
+        dRow.innerHTML = '';
+        diceContainer.appendChild(dRow);
+        let dRow2 = document.createElement('div');
+        dRow2.setAttribute('class', 'boardRow');
+        dRow2.setAttribute('id', 'boardRow2');
+        dRow2.innerHTML = '';
+        diceContainer.appendChild(dRow2);
+    } else {
+        let dRow = document.createElement('div');
+        dRow.setAttribute('class', 'boardRow');
+        dRow.setAttribute('id', 'boardRow1');
+        dRow.innerHTML = '';
+        diceContainer.appendChild(dRow);
+    };
+
     for (i = 0; i < diceNumber; i++) {
-        let roll = randomDice(1,9);
+        let roll = randomDice(1, diceMaxDots);
         let d = document.createElement('div');
         d.setAttribute('class', 'dice');
         d.innerHTML = '';
@@ -62,7 +89,6 @@ function diceRoll() {
                 break;
         };
 
-
         for (k = 0; k < 49; k++) {
             let e = document.createElement('div');
             e.innerHTML = '';
@@ -72,6 +98,12 @@ function diceRoll() {
             d.appendChild(e);
         };
 
-        diceContainer.appendChild(d);
+        if (i < 2) {
+            let r1 = document.getElementById('boardRow1');
+            r1.appendChild(d);
+        } else {
+            let r2 = document.getElementById('boardRow2');
+            r2.appendChild(d);
+        };
     };
-}
+};
